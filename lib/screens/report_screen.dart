d85cb6a40f849bc7be55b34e7f61d8b97c0c42b8
bomkeen <bomkeendata@gmail.com>
2018-10-30 14:07:12 +0700

    'report'

--- lib/screens/report_screen.dart
+++ lib/screens/report_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:ab_money/utils/database_helper.dart';
 
 class ReportScreen extends StatefulWidget {
   @override
@@ -6,8 +8,65 @@ class ReportScreen extends StatefulWidget {
 }
 
 class _ReportScreenState extends State<ReportScreen> {
+  DatabaseHelper databaseHelper = DatabaseHelper.internal();
+  List items = [];
+  Future getItems() async {
+    var res = await databaseHelper.getList();
+    print(res);
+    setState(() {
+      items = res;
+    });
+  }
+
+  void initState() {
+    super.initState();
+    getItems();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+//      appBar: AppBar(
+//        title: Text('Member'),
+//        actions: <Widget>[
+//          IconButton(icon: Icon(Icons.photo_camera), onPressed: () {})
+//        ],
+//      ),
+      body: ListView.builder(
+          itemBuilder: (context, int index) {
+            return ListTile(
+//                onTap: () async {
+//                  var response = await Navigator.push(
+//                      context,
+//                      MaterialPageRoute(
+//                          builder: (context) =>
+//                              AddMemberScreen(members[index]['id'])));
+//                  if (response != null) {
+//                    getMembers();
+//                  }
+//                },
+                title: Text('${items[index]['date']}'),
+                subtitle: Text(
+                    '${items[index]['income']}----${items[index]['outcome']}'),
+                trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+//                      removeMembers(members[index]['id']);
+                    }));
+          },
+          itemCount: items != null ? items.length : 0),
+      floatingActionButton: FloatingActionButton(
+        //// ใช้แบบนี เพราะมีการส่งค่า กลับบมา ตอนกด save
+        onPressed: () async {
+//          var response = await Navigator.push(context,
+//              MaterialPageRoute(builder: (context) => AddMemberScreen(null)));
+//          if (response != null) {
+//            getMembers();
+//          }
+        },
+        child: Icon(Icons.person_add),
+      ),
+    );
   }
 }
